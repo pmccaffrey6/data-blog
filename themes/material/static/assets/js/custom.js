1d5a1c94065c680800f77f76558927aa3295ea06
Peter McCaffrey <pmccaffrey@tests-MBP-2.lan>
2017-07-18 15:18:12 -0500

    fluid images

--- themes/material/static/assets/js/custom.js
+++ themes/material/static/assets/js/custom.js
@@ -1,16 +1,21 @@
 // Set output_png to center content
 $(document).ready( function() {
-  //center images
-  var pngs = Array.from(document.getElementsByClassName("output_png"))
-  pngs.map(function(elem) {elem.className += " row justify-content-center"})
+  //center and autoscale images
+  var pngs = Array.from(document.getElementsByClassName("output_png"));
+
+  pngs.map(function(elem) {
+    $(elem).addClass("container");
+  });
+
+  $('.output_png img').map(function(k,v) { $(v).addClass('img-fluid mx-auto d-block') } );
 
   //also center html output such as tables and Bokeh charts
-  var htmlElems = Array.from(document.getElementsByClassName("output_html"))
-  htmlElems.map(function(elem) {elem.className += " row justify-content-center"})
+  var htmlElems = Array.from(document.getElementsByClassName("output_html"));
+  htmlElems.map(function(elem) {elem.className += " row justify-content-center"});
 
   //also apply this to bk-root class for Bokeh plots
-  var bkRoot = Array.from(document.getElementsByClassName("bk-root"))
-  bkRoot.map(function(elem) {elem.className += " row justify-content-center"})
+  var bkRoot = Array.from(document.getElementsByClassName("bk-root"));
+  bkRoot.map(function(elem) {elem.className += " row justify-content-center"});
 
   // nanobar.js
   var options = {
